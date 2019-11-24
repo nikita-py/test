2bd6bdba84e5700f436d5ac53a320a823c644894
nikita-py <nborshov8gmail.com>
2019-11-24 23:41:10 +0200

    testcommmit

---
+++ venv/nik_test/test.py
@@ -0,0 +1,12 @@
+import random
+def usthis():
+    n = str(input("Choose name :"))
+    print(n)
+    n1 = str(input("Choose surename :"))
+    print(n1)
+    print("Hello user - " , n , n1)
+
+
+
+
+
