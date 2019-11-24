479a43d7ad5c505ff3588208215c0ed77dd47c86
nikita-py <nborshov8gmail.com>
2019-11-24 23:52:50 +0200

    sdfghjk

--- venv/nik_test/test.py
+++ venv/nik_test/test.py
@@ -2,7 +2,7 @@ import random
 def usthis():
     n = str(input("Choose name :"))
     print(n)
-    n1 = str(input("Choose surename :"))
+    n1 = str(input("Choose surenamedd :"))
     print(n1)
     print("Hello user - " , n , n1)
 
